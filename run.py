79533bc317b9217566a380aba51bf6c9b9a54407
Mitul Khemani <mitulkhemani@gmail.com>
2026-01-04 16:41:22 +0530

    checked for TITAGARH share

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from predict.predict import predict_model
 
 DATA_PATH = "data/stock_long_data.csv"
 MODEL_PATH = "model/model.keras"
-STOCK = "TMCV.NS"
+STOCK = "TITAGARH.NS"
 DAYS = "7d"
 PREDICT_DAY = "1d"
 INTERVAL = "1m"
