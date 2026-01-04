9403b627af09079425293cd8eecc66f6e3a1e06d
Mitul Khemani <mitulkhemani@gmail.com>
2026-01-04 16:35:43 +0530

    window size for prediction

--- predict/predict.py
+++ predict/predict.py
@@ -4,15 +4,12 @@ import numpy as np
 import pandas as pd
 
 #PREDICTING THE NEXT MINUTE CLOSE PRICE USING THE LAST FETCHED DATASET
-def predict_model(STOCK, DAYS, INTERVAL, DATA_PATH, MODEL_PATH):
+def predict_model(STOCK, DAYS, INTERVAL, WINDOW_SIZE, DATA_PATH, MODEL_PATH):
     fetch_long_term_data(STOCK=STOCK, days=DAYS, interval=INTERVAL, DATA_PATH=DATA_PATH)
     df = pd.read_csv(DATA_PATH)
-    df = df.tail(1)
-    High = float(df['High'].values[0])
-    Low = float(df['Low'].values[0])
-    Open = float(df['Open'].values[0])
-    Volume = float(df['Volume'].values[0])
-    input_data = np.array([[[Open, High, Low, Volume]]])
+    df = df.tail(WINDOW_SIZE)
+    input_data = df[['Open', 'High', 'Low', 'Volume']].values
+    input_data = input_data.reshape((1, WINDOW_SIZE, 4)).astype(np.float32)
     Predicted_Value = predict_next_minute_close(MODEL_PATH, input_data)
     print("Predicted Next Minute Close Price: ", Predicted_Value[0][0])
     return Predicted_Value[0][0]
