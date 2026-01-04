a9ade2e3262a171e381b900cd7d7511d1202f684
Mitul Khemani <mitulkhemani@gmail.com>
2026-01-04 13:36:23 +0530

    data sorting

--- data/clean.py
+++ data/clean.py
@@ -6,5 +6,7 @@ def clean_data(PATH):
     df.reset_index(drop=True, inplace=True)
     df.rename(columns = {'Price': 'Date_Time'}, inplace=True)
     df['Date_Time'] = pd.to_datetime(df['Date_Time'])
+    df.sort_values('Date_Time', inplace=True)
+    df.reset_index(drop = True, inplace=True)
     df.to_csv(PATH, index=False)
     return PATH
