6b23135504bf07d69a4e9759b5f1c333dc9337ce
Mitul Khemani <mitulkhemani@gmail.com>
2026-01-04 13:29:05 +0530

    data cleaning improved

--- data/clean.py
+++ data/clean.py
@@ -4,5 +4,7 @@ def clean_data(PATH):
     df = pd.read_csv(PATH)
     df = df.iloc[2:,:]
     df.reset_index(drop=True, inplace=True)
+    df.rename(columns = {'Price': 'Date_Time'}, inplace=True)
+    df['Date_Time'] = pd.to_datetime(df['Date_Time'])
     df.to_csv(PATH, index=False)
     return PATH
